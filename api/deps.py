1383dc2de193aa50eba38d9277f7def5afdad1fb
Nikita Borodenko <77574092+EXG1O@users.noreply.github.com>
2026-08-11 23:45:02 +0300

    Replace `hmac` lib to `secrets` lib for `compare_digest` func in `api.deps` module (#3)

--- api/deps.py
+++ api/deps.py
@@ -5,7 +5,7 @@ from fastapi.security import APIKeyHeader
 from core.settings import APP_TOKEN, TELEGRAM_TOKEN
 
 from typing import Annotated
-import hmac
+import secrets
 
 telegram_token_header = APIKeyHeader(name='X-Telegram-Bot-Api-Secret-Token')
 app_token_header = APIKeyHeader(name='X-API-KEY')
@@ -14,12 +14,12 @@ app_token_header = APIKeyHeader(name='X-API-KEY')
 async def verify_telegram_token(
     token: Annotated[str, Depends(telegram_token_header)],
 ) -> str:
-    if not hmac.compare_digest(token, TELEGRAM_TOKEN):
+    if not secrets.compare_digest(token, TELEGRAM_TOKEN):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return token
 
 
 async def verify_app_token(token: Annotated[str, Depends(app_token_header)]) -> str:
-    if not hmac.compare_digest(token, APP_TOKEN):
+    if not secrets.compare_digest(token, APP_TOKEN):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return token
